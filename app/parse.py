e4f6cb2858e8f3ed66340637c65563987d1a56ad
Oleksandr Barabanov <imtence@icloud.com>
2025-03-27 22:33:38 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,13 +1,12 @@
 import csv
+import time
 from dataclasses import dataclass
 from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
 
-
 BASE_URL = "https://quotes.toscrape.com/"
-
 
 @dataclass
 class Quote:
@@ -20,10 +19,6 @@ class Quote:
         text, author, tags_str = row
         tags = tags_str.split(", ") if tags_str else []
         return cls(text, author, tags)
-
-
-QUOTE_FIELDS = ["text", "author", "tags"]
-
 
 def get_quotes() -> list[Quote]:
     quotes = []
@@ -52,18 +47,20 @@ def get_quotes() -> list[Quote]:
             quotes.append(Quote(text, author, tags))
 
         page += 1
+        time.sleep(1)
 
     return quotes
-
 
 def write_quotes_to_csv(quotes: list[Quote], output_csv_path: str) -> None:
     with open(output_csv_path, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(QUOTE_FIELDS)
+        writer.writerow(["text", "author", "tags"])
         for quote in quotes:
             writer.writerow([quote.text, quote.author, ", ".join(quote.tags)])
-
 
 def main(output_csv_path: str) -> None:
     quotes = get_quotes()
     write_quotes_to_csv(quotes, output_csv_path)
+
+if __name__ == "__main__":
+    main("quotes.csv")
